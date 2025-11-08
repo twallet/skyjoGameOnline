3557735c6771e8fd85548952cda7a88277a37b4c
Thomas Wallet <twallet@gmail.com>
2025-11-08 19:33:03 -0300

    Ejercicio 3

--- 1.arrow-let-op/practica.js
+++ 1.arrow-let-op/practica.js
@@ -1,6 +1,8 @@
 /* Ejercicio 1 - Crear cartas con funciones flecha y variables
 Define una función flecha para crear una carta con un valor dado. La función debe devolver un objeto que represente la carta con su valor. */
 
+console.log("################# EJERCICIO 1 #################");
+
 class Card {
   constructor(value) {
     this._cardValue = value;
@@ -19,6 +21,8 @@ console.log("Card value: " + card5.value);
 
 /*Ejercicio 2 - Definir el mazo básico usando let y const
 Define una constante mazo como un array vacío. Luego, usando una variable cantidadCartas, agrega al mazo cartas con un valor específico según las reglas del juego. Usa un ciclo para agregar varias cartas.*/
+
+console.log("################# EJERCICIO 2 #################");
 
 const SKYJO = {
   values: [-2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
@@ -60,3 +64,14 @@ class Deck {
 var deck = Deck.generateDeck(SKYJO);
 console.log("Deck length: " + deck.cardsDeck.length);
 console.log("Deck cards: [" + deck.show() + "]");
+
+/* Ejercicio 3 - Uso de operadores para validar cartas válidas
+Escribe una función flecha que reciba una carta y devuelva true si su valor está dentro del rango válido para las cartas de Skyjo (por ejemplo, entre -2 y 12), o false si no. */
+
+console.log("################# EJERCICIO 3 #################");
+
+var validCard = card => {return SKYJO.values.includes(card.value)};
+var card8 = new Card(8);
+var card15 = new Card(15);
+console.log("8: " + validCard(card8));
+console.log("15: " + validCard(card15));
